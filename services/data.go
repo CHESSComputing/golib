100d59908328e6e8b319e5b4436e545c2c0c6e8b
Valentin Kuznetsov <vkuznet@gmail.com>
2023-12-19 10:42:59 -0500

    add response structs

--- services/data.go
+++ services/data.go
@@ -1,9 +1,37 @@
 package server
 
-import "github.com/CHESSComputing/golib/mongo"
+import (
+	"github.com/CHESSComputing/golib/mongo"
+)
 
 // MetaRecord represents meta-data record used for injection
 type MetaRecord struct {
 	Schema string
 	Record mongo.Record
+}
+
+// MetaResponse represents response struct from meta-data service
+type MetaResponse struct {
+	Query    string
+	Spec     any
+	Idx      int
+	Limit    int
+	NRecords int
+	Records  []mongo.Record
+}
+
+// DBSResponse represents reponse struct from dbs service
+type DBSResponse struct {
+	Query    string
+	SQL      string
+	Idx      int
+	Limit    int
+	NRecords int
+	Records  []mongo.Record
+}
+
+// DiscoveryResponse represents response struct from discovery service
+type DiscoveryResponse struct {
+	DBSResponse  `json:"dbs"`
+	MetaResponse `json:"meta"`
 }
