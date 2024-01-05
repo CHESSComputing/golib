e6de1008a8ab5facc5675f9fe62a48d1a1f4d916
Valentin Kuznetsov <vkuznet@gmail.com>
2024-01-05 10:00:05 -0500

    Add log management and gin options settings

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"fmt"
 	"io/fs"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -10,6 +11,8 @@ import (
 	authz "github.com/CHESSComputing/golib/authz"
 	srvConfig "github.com/CHESSComputing/golib/config"
 	"github.com/gin-gonic/gin"
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
+	logging "github.com/vkuznet/http-logging"
 )
 
 var _routes gin.RoutesInfo
@@ -22,12 +25,50 @@ type Route struct {
 	Authorized bool
 }
 
+// InitServer provides server initialization
+func InitServer(webServer srvConfig.WebServer) {
+	// setup log options
+	rotateLogs(webServer.LogFile)
+
+	// setup log options
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	if webServer.LogLongFile {
+		log.SetFlags(log.LstdFlags | log.Llongfile)
+	}
+
+	// setup gin options
+	if webServer.GinOptions.DisableConsoleColor {
+		// Disable Console Color
+		gin.DisableConsoleColor()
+	}
+
+	// set gin mode of operation
+	if webServer.GinOptions.Mode == "release" {
+		gin.SetMode(gin.ReleaseMode)
+	} else if webServer.GinOptions.Mode == "debug" {
+		gin.SetMode(gin.DebugMode)
+	} else if webServer.GinOptions.Mode == "test" {
+		gin.SetMode(gin.TestMode)
+	}
+
+	// use production flag to overwrite gin mode
+	if webServer.GinOptions.Production {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	log.Printf("webServer configuration:\n%s", webServer.String())
+}
+
 // Router provids server router, it takes two maps:
 // one for non-authorized routes and anotehr for authorized ones
-func Router(routes []Route, fsys fs.FS, static, base string, verbose int) *gin.Engine {
-	// Disable Console Color
-	// gin.DisableConsoleColor()
-	r := gin.Default()
+func Router(routes []Route, fsys fs.FS, static string, webServer srvConfig.WebServer) *gin.Engine {
+	base := webServer.Base
+	verbose := webServer.Verbose
+
+	InitServer(webServer)
+
+	// setup gin router
+	//     r := gin.Default()
+	r := gin.New()
 
 	// GET routes
 	r.GET("/apis", ApisHandler)
@@ -89,4 +130,33 @@ func Router(routes []Route, fsys fs.FS, static, base string, verbose int) *gin.E
 	_routes = r.Routes()
 
 	return r
+}
+
+// helper function to rotate logs
+func rotateLogs(srvLogName string) {
+	if srvLogName != "" {
+		log.SetOutput(new(logging.LogWriter))
+		rl, err := rotatelogs.New(logName(srvLogName))
+		if err == nil {
+			rotlogs := logging.RotateLogWriter{RotateLogs: rl}
+			log.SetOutput(rotlogs)
+		}
+	}
+}
+
+// logName returns proper log name based on Config LogFile and either
+// hostname or pod name (used in k8s environment).
+func logName(srvLogName string) string {
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Println("unable to get hostname", err)
+	}
+	if os.Getenv("MY_POD_NAME") != "" {
+		hostname = os.Getenv("MY_POD_NAME")
+	}
+	logName := srvLogName + "_%Y%m%d"
+	if hostname != "" {
+		logName = fmt.Sprintf("%s_%s", srvLogName, hostname) + "_%Y%m%d"
+	}
+	return logName
 }
