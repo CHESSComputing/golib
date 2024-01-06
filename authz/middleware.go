49dcfd82b65f4cb7d01db0e530d23dee64e6ba35
Valentin Kuznetsov <vkuznet@gmail.com>
2024-01-06 08:33:42 -0500

    Standarize error output via services.Response record; remove obsolete code

--- authz/middleware.go
+++ authz/middleware.go
@@ -1,17 +1,15 @@
 package auth
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"strings"
 
 	srvConfig "github.com/CHESSComputing/golib/config"
+	services "github.com/CHESSComputing/golib/services"
 	"github.com/gin-gonic/gin"
-	jwt "github.com/golang-jwt/jwt/v4"
 )
 
 // _token is used across all authorized APIs
@@ -29,8 +27,8 @@ func TokenMiddleware(clientId string, verbose int) gin.HandlerFunc {
 		if err := token.Validate(clientId); err != nil {
 			msg := fmt.Sprintf("TokenMiddleware: invalid token %s, error %v", tokenStr, err)
 			log.Println("ERROR:", msg)
-			c.AbortWithStatusJSON(
-				http.StatusUnauthorized, gin.H{"status": "fail", "error": err.Error()})
+			rec := services.Response("authz", http.StatusUnauthorized, services.TokenError, err)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, rec)
 			return
 		}
 		if verbose > 0 {
@@ -49,8 +47,8 @@ func ScopeTokenMiddleware(scope, clientId string, verbose int) gin.HandlerFunc {
 		if err := token.Validate(clientId); err != nil {
 			msg := fmt.Sprintf("ScopeTokenMiddleware: invalid token %s, error %v", tokenStr, err)
 			log.Println("ERROR:", msg)
-			c.AbortWithStatusJSON(
-				http.StatusUnauthorized, gin.H{"status": "fail", "error": err.Error()})
+			rec := services.Response("authz", http.StatusUnauthorized, services.TokenError, err)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, rec)
 			return
 		}
 		if verbose > 0 {
@@ -62,8 +60,8 @@ func ScopeTokenMiddleware(scope, clientId string, verbose int) gin.HandlerFunc {
 			msg := fmt.Sprintf("ScopeTokenMiddleware: token scope '%s' does not match with scope '%s'", token.Scope, scope)
 			log.Println("ERROR:", msg)
 			log.Println("token", tokenStr)
-			c.AbortWithStatusJSON(
-				http.StatusUnauthorized, gin.H{"status": "fail", "error": msg})
+			rec := services.Response("authz", http.StatusUnauthorized, services.ScopeError, errors.New(msg))
+			c.AbortWithStatusJSON(http.StatusUnauthorized, rec)
 			return
 		}
 		c.Next()
@@ -79,108 +77,4 @@ func RequestToken(r *http.Request) string {
 	arr := strings.Split(tokenStr, " ")
 	token := arr[len(arr)-1]
 	return token
-}
-
-// helper function to refresh global token used in authorized APIs
-func refreshToken() error {
-	// check and obtain token
-	var err error
-	if _token == nil {
-		if token, err := getToken(); err == nil {
-			_token = &token
-		} else {
-			return err
-		}
-	} else {
-		err = _token.Validate(srvConfig.Config.Authz.ClientID)
-	}
-	return err
-}
-
-// helper function to obtain JWT token from Authz service
-func getToken() (Token, error) {
-	var token Token
-	rurl := fmt.Sprintf(
-		"%s/oauth/token?client_id=%s&client_secret=%s&grant_type=client_credentials&scope=read",
-		srvConfig.Config.Services.AuthzURL,
-		srvConfig.Config.Authz.ClientID,
-		srvConfig.Config.Authz.ClientSecret)
-	resp, err := http.Get(rurl)
-	defer resp.Body.Close()
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return token, err
-	}
-	err = json.Unmarshal(data, &token)
-	if err != nil {
-		return token, err
-	}
-	reqToken := token.AccessToken
-	if srvConfig.Config.Authz.Verbose > 0 {
-		log.Printf("INFO: obtain token %+v", token)
-	}
-
-	// validate our token
-	var jwtKey = []byte(srvConfig.Config.Authz.ClientID)
-	claims := &Claims{}
-	tkn, err := jwt.ParseWithClaims(reqToken, claims, func(token *jwt.Token) (any, error) {
-		return jwtKey, nil
-	})
-	if err != nil {
-		if err == jwt.ErrSignatureInvalid {
-			return token, errors.New("invalid token signature")
-		}
-		return token, err
-	}
-	if !tkn.Valid {
-		log.Println("WARNING: token invalid")
-		return token, errors.New("invalid token validity")
-	}
-	return token, nil
-}
-
-// gin cookies
-// https://gin-gonic.com/docs/examples/cookie/
-// more advanced use-case:
-// https://stackoverflow.com/questions/66289603/use-existing-session-cookie-in-gin-router
-func AuthMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// check if our user key is set
-		if user, err := c.Cookie("user"); err == nil {
-			if srvConfig.Config.Authz.Verbose > 0 {
-				log.Println(c.Request.Method, c.Request.URL.Path, user)
-			}
-			c.Set("user", user)
-			if err := refreshToken(); err != nil {
-				c.SetCookie("user", "", -1, "/", "localhost", false, true)
-				c.Set("user", "")
-				c.Data(http.StatusUnauthorized, "text/html; charset=utf-8", []byte("Unauthorized access"))
-
-				//                 content := errorTmpl(c, "unable to get valid token", err)
-				//                 log.Fatal(content)
-
-				//                 c.Data(http.StatusUnauthorized, "text/html; charset=utf-8", []byte(content))
-				//                 c.Redirect(http.StatusFound, "/")
-				return
-			}
-			return
-		}
-
-		if user, ok := c.Get("user"); !ok {
-			if srvConfig.Config.Authz.Verbose > 0 {
-				log.Println(c.Request.Method, c.Request.URL.Path)
-			}
-			c.Redirect(http.StatusFound, "/login")
-		} else {
-			if srvConfig.Config.Authz.Verbose > 0 {
-				log.Println(c.Request.Method, c.Request.URL.Path, user)
-			}
-		}
-		if err := refreshToken(); err != nil {
-			//             content := errorTmpl(c, "unable to get valid token", err)
-			c.Data(http.StatusUnauthorized, "text/html; charset=utf-8", []byte("Unauthorized access"))
-			return
-		}
-		c.Next()
-	}
 }
