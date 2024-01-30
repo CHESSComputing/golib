9d47d09fba029069581fe00b9b1bc9e8cb0300bd
Valentin Kuznetsov <vkuznet@gmail.com>
2024-01-30 14:19:44 -0500

    New zenodo unit test placeholder

---
+++ zenodo/zenodo_test.go
@@ -0,0 +1,14 @@
+package zenodo
+
+import (
+	"testing"
+)
+
+// TestZenodo
+func TestZenodo(t *testing.T) {
+	// TODO: fill with something meaningful
+	a := "test"
+	if a != "test" {
+		t.Error("Fail to parse local variable")
+	}
+}
