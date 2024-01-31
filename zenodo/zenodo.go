9e77b29b8f4fc9b389d85ec06ce520a5a0242faf
Valentin Kuznetsov <vkuznet@gmail.com>
2024-01-31 15:09:06 -0500

    Add more info to structs

--- zenodo/zenodo.go
+++ zenodo/zenodo.go
@@ -1,20 +1,64 @@
 package zenodo
 
-// ZenodoError represents individual zenodo error struct
-type ZenodoError struct {
+import "errors"
+
+// Some info are reverse engineered from the followin resources:
+// https://felipecrp.com/2021/01/01/uploading-to-zenodo-through-api.html
+// Zenodo REST API: https://developers.zenodo.org/
+// snd, some discussion about zenodo APIs:
+// https://github.com/zenodo/zenodo/issues/2168
+
+// Creator represents creator record
+type Creator struct {
+	Name       string `json:"name"`
+	Affliation string `json:"affiliation"`
+}
+
+// MetaDataRecord represents meta-data record
+type MetaDataRecord struct {
+	PublicationType string    `json:"publication_type"`
+	UploadType      string    `json:"upload_type"`
+	Description     string    `json:"description"`
+	Keywords        []string  `json:"keywords"`
+	Title           string    `json:"title"`
+	Licences        []string  `json:"licenses,omitempty"`
+	Version         string    `json:"version,omitempty"`
+	Publisher       string    `json:"publisher,omitempty"`
+	Contributors    []Creator `json:"contributors,omitempty"`
+	Creators        []Creator `json:"creators"`
+}
+
+// Validate provides validation of meta data record
+func (m *MetaDataRecord) Validate() error {
+	if m.PublicationType == "" {
+		return errors.New("missing publication type, e.g. article")
+	} else if m.UploadType == "" {
+		return errors.New("missing upload type, e.g. publication")
+	} else if m.Description == "" {
+		return errors.New("missing description")
+	} else if m.Title == "" {
+		return errors.New("missing title")
+	} else if len(m.Creators) == 0 {
+		return errors.New("missing creators, e.g. [{\"name\":\"First Last\", \"affiliation\": \"Zenodo\"}]")
+	}
+	return nil
+}
+
+// Error represents individual zenodo error struct
+type Error struct {
 	Field    string
 	Messages []string
 }
 
-// ZenodoReponse represents zenodo response
-type ZenodoResponse struct {
+// Reponse represents zenodo response
+type Response struct {
 	Status  int
 	Message string
-	Error   []ZenodoError
+	Error   []Error
 }
 
-// ZenodoCreatedResponse represents output of /create API
-type ZenodoCreatedResponse struct {
+// CreateResponse represents output of /create API
+type CreateResponse struct {
 	Id        int64    `json:"id"`
 	MetaData  MetaData `json:"metadata"`
 	Created   string   `json:"created"`
@@ -24,7 +68,7 @@ type ZenodoCreatedResponse struct {
 	State     string   `json:"state"`
 	Submitted bool     `json:"submitted"`
 	Title     string   `json:"title"`
-	Links     Links    `json:"self"`
+	Links     Links    `json:"links"`
 }
 
 // PrereserveDoi represents PrereserveDoi struct
@@ -41,7 +85,7 @@ type MetaData struct {
 
 // Links contains zenodo links
 type Links struct {
-	Badge              string `json:"badge"`
+	Bucket             string `json:"bucket"`
 	Discard            string `json:"discard"`
 	Edit               string `json:"edit"`
 	Files              string `json:"files"`
@@ -54,8 +98,8 @@ type Links struct {
 	Self               string `json:"self"`
 }
 
-// ZenodoAddResponse represents output of /create API
-type ZenodoAddResponse struct {
+// AddResponse represents output of /create API
+type AddResponse struct {
 	Created  string `json:"created"`
 	Modified string `json:"modified"`
 	Size     int64  `json:"size"`
@@ -64,5 +108,5 @@ type ZenodoAddResponse struct {
 	Checksum string `json:"checksum"`
 	Owner    int    `json:"owner"`
 	RecordId int64  `json:"record_id"`
-	Links    Links  `json:"self"`
+	Links    Links  `json:"links"`
 }
