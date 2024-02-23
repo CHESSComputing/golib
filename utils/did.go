99c038c938350bcea3c2fa0724dd6e408560bdf8
Valentin Kuznetsov <vkuznet@gmail.com>
2024-02-23 09:20:12 -0500

    new code for did

---
+++ utils/did.go
@@ -0,0 +1,52 @@
+package utils
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+// DIDKeys provide sorted, lower-case list of did keys from comma separated list of attributes
+func DIDKeys(attrs string) []string {
+	if attrs == "" {
+		attrs = "btr,beamline,cycle,sample"
+	}
+	attrs = strings.Replace(attrs, " ", "", -1)
+	var keys []string
+	for _, k := range strings.Split(attrs, ",") {
+		if k != "" {
+			keys = append(keys, strings.ToLower(k))
+		}
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+func CreateDID(rec map[string]any, attrs, sep, div string) string {
+	didKeys := DIDKeys(attrs)
+	var did string
+	for _, k := range didKeys {
+		v, ok := rec[k]
+		if !ok {
+			continue
+		}
+		key := strings.ToLower(k)
+		var val string
+		switch vvv := v.(type) {
+		case []string:
+			val = strings.ToLower(fmt.Sprintf("%v", strings.Join(vvv, ",")))
+		case []any:
+			var arr []string
+			for _, i := range vvv {
+				arr = append(arr, fmt.Sprintf("%v", i))
+			}
+			val = strings.ToLower(fmt.Sprintf("%v", strings.Join(arr, ",")))
+		default:
+			val = strings.ToLower(fmt.Sprintf("%v", vvv))
+		}
+		if InList(key, didKeys) {
+			did = fmt.Sprintf("%s%s%s%s%v", did, sep, key, div, val)
+		}
+	}
+	return did
+}
---
+++ utils/did_test.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"testing"
+)
+
+// TestDIDKeys
+func TestDIDKeys(t *testing.T) {
+	keys := DIDKeys(", a, b, c, ")
+	if keys[0] != "a" || keys[1] != "b" || keys[2] != "c" {
+		t.Error("Fail TestDIDKeys")
+	}
+}
+
+// TestCreateDID
+func TestCreateDID(t *testing.T) {
+	rec := make(map[string]any)
+	rec["foo"] = 1
+	rec["bla"] = "value"
+	rec["arr"] = []int{1, 2, 3}
+	attrs := "bla,foo"
+	sep := "/"
+	div := ":"
+	did := CreateDID(rec, attrs, sep, div)
+	expect := "/bla:value/foo:1"
+	if did != expect {
+		t.Errorf("Fail TestCreateDID did=%s, expect=%s\n", did, expect)
+	}
+}
