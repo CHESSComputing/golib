2e1563db49c11a574155dc76ab6f6f3970576daa
Valentin Kuznetsov <vkuznet@gmail.com>
2024-04-03 15:37:15 -0400

    New code for LDAP cache

---
+++ ldap/structs.go
@@ -0,0 +1,58 @@
+package ldap
+
+import (
+	"errors"
+	"time"
+
+	srvConfig "github.com/CHESSComputing/golib/config"
+)
+
+// Entry represents LDAP user entry
+type Entry struct {
+	DN     string
+	Groups []string
+	Expire time.Time
+}
+
+// Cache represent LDAP cache
+type Cache struct {
+	Map    map[string]Entry
+	Expire string
+}
+
+// Search provides cached search results
+func (c *Cache) Search(login, password, user string) (Entry, error) {
+	if srvConfig.Config == nil {
+		srvConfig.Init()
+	}
+	if c.Map == nil {
+		c.Map = make(map[string]Entry)
+	}
+	ldapURL := srvConfig.Config.LDAP.URL
+	baseDN := srvConfig.Config.LDAP.BaseDN
+	attributes := []string{"memberOf"}
+	if cacheEntry, ok := c.Map[user]; ok {
+		if cacheEntry.Expire.Before(time.Now()) {
+			return cacheEntry, nil
+		}
+	}
+	results, err := Search(ldapURL, login, password, baseDN, user, attributes)
+	if err != nil {
+		return Entry{}, err
+	}
+	for _, entry := range results.Entries {
+		for _, attr := range entry.Attributes {
+			// here attr.Name is our attribute name, e.g. memberOf
+			cacheEntry := Entry{
+				DN:     entry.DN,
+				Groups: attr.Values,
+				Expire: time.Now(),
+			}
+			// here we suppose to have only eny entry per user filled with groups
+			c.Map[user] = cacheEntry
+			return cacheEntry, nil
+		}
+	}
+	// we should not reach this point
+	return Entry{}, errors.New("no cache entry")
+}
