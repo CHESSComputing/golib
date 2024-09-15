23930df5db6add4c8a7904abfd07a572721e26d3
Valentin Kuznetsov <vkuznet@gmail.com>
2024-09-15 09:51:40 -0400

    Add globus exception for test

--- go_test.sh
+++ go_test.sh
@@ -3,7 +3,7 @@
 set -e
 echo "mode: atomic" > coverage.txt
 
-skipServices="zenodo ldap schema"
+skipServices="zenodo ldap schema globus"
 
 for d in $(go list ./... | grep -v vendor); do
     echo "Testing $d"
