5ebe89b76440ba7029fbb81600f5f75b72140909
Valentin Kuznetsov <vkuznet@gmail.com>
2024-12-20 07:42:26 -0500

    Add metadata to attributes

--- datacite/data.go
+++ datacite/data.go
@@ -76,6 +76,8 @@ type Attributes struct {
 	Registered           time.Time     `json:"registered"`
 	Published            string        `json:"published"`
 	Updated              time.Time     `json:"updated"`
+
+	MetaData any `json:"metaData"`
 }
 
 // Creator represents creator struct
