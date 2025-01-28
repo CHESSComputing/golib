35737e610262eefb3abfebe41d01d918a999e442
Valentin Kuznetsov <vkuznet@gmail.com>
2025-01-28 11:07:00 -0500

    change port

--- services/helpers_test.go
+++ services/helpers_test.go
@@ -7,11 +7,11 @@ import (
 // TestHTTPResponse
 func TestHTTPResponse(t *testing.T) {
 	httpReq := NewHttpRequest("scope", 0)
-	resp, err := httpReq.Get("http://localhost:1234")
+	resp, err := httpReq.Get("http://localhost:12345")
 	if err == nil {
-		t.Error("Fail to process http request")
+		t.Errorf("Fail to process http request, err=%v\n", err)
 	}
 	if resp != nil {
-		t.Error("HTTP response is not nil")
+		t.Errorf("HTTP response is not nil, resp=%v\n", resp)
 	}
 }
