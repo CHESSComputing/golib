4c9ca8ff91bb32487c3304c73629ee37094b34c2
Valentin Kuznetsov <vkuznet@gmail.com>
2025-02-13 08:36:58 -0500

    Set default DataLocationAttributes if they are not provided

--- config/config.go
+++ config/config.go
@@ -431,6 +431,12 @@ func ParseConfig(cfile string) (SrvConfig, error) {
 	if config.LDAP.Expire == 0 {
 		config.LDAP.Expire = 3600 // LDAP cache records expire in 1 hour
 	}
+	if len(config.CHESSMetaData.DataLocationAttributes) == 0 {
+		config.CHESSMetaData.DataLocationAttributes = []string{"data_location_raw", "btr_location_raw"}
+	}
+	if len(config.MetaData.DataLocationAttributes) == 0 {
+		config.MetaData.DataLocationAttributes = []string{"data_location_raw", "btr_location_raw"}
+	}
 	return config, nil
 }
 
