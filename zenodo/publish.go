0b9784f721defa6286adf845ed2618c3278626ea
Valentin Kuznetsov <vkuznet@gmail.com>
2025-03-27 16:04:06 -0400

    code refactoring to use direct call to Zenodo REST APIs

--- zenodo/publish.go
+++ zenodo/publish.go
@@ -6,165 +6,190 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"strings"
 
 	srvConfig "github.com/CHESSComputing/golib/config"
-	"github.com/CHESSComputing/golib/services"
 )
 
-var _httpReadRequest, _httpWriteRequest *services.HttpRequest
 var Verbose int
-
-func initSrv() {
-	if _httpReadRequest == nil {
-		_httpReadRequest = services.NewHttpRequest("read", Verbose)
-	}
-	if _httpWriteRequest == nil {
-		_httpWriteRequest = services.NewHttpRequest("write", Verbose)
-	}
-	_httpReadRequest.GetToken()
-	_httpWriteRequest.GetToken()
-}
-
-// helper function to check response status
-func checkResponse(resp *http.Response) error {
-	if resp.StatusCode < 200 || resp.StatusCode >= 400 {
-		// read response body to get error
-		if data, err := io.ReadAll(resp.Body); err == nil {
-			msg := fmt.Sprintf("call to upstream service not successfull, %s", string(data))
-			return errors.New(msg)
-		}
-		msg := fmt.Sprintf("call to upstream service fails with code %d", resp.StatusCode)
-		return errors.New(msg)
-	}
-	return nil
-}
 
 // CreateRecord provides create record API
 func CreateRecord(payload []byte) (CreateResponse, error) {
-	// init reader/writer and srv config
-	initSrv()
-
-	var doc CreateResponse
-	// create new DOI resource
-	rurl := fmt.Sprintf("%s/create", srvConfig.Config.Services.PublicationURL)
-	resp, err := _httpWriteRequest.Post(rurl, "application/json", bytes.NewBuffer(payload))
-	defer resp.Body.Close()
+	/*
+	 curl --request POST 'https://zenodo.org/api/deposit/depositions?access_token=<KEY>' \
+	 --header 'Content-Type: application/json'  \
+	 --data-raw '{}'
+	*/
+	var response CreateResponse
+	// create new deposit
+	zurl := srvConfig.Config.DOI.Zenodo.Url
+	token := srvConfig.Config.DOI.Zenodo.AccessToken
+	rurl := fmt.Sprintf("%s/deposit/depositions?access_token=%s", zurl, token)
+	req, err := http.NewRequest("POST", rurl, bytes.NewBuffer(payload))
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
-		return doc, err
-	}
-	if err := checkResponse(resp); err != nil {
-		return doc, err
-	}
-
-	// capture response and extract document id (did)
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return doc, err
-	}
-	err = json.Unmarshal(data, &doc)
-	if err != nil {
-		return doc, err
-	}
-	return doc, nil
-}
-
-func getBid(did int64) (string, error) {
-	rurl := fmt.Sprintf("%s/docs/%d", srvConfig.Config.Services.PublicationURL, did)
-	resp, err := _httpReadRequest.Get(rurl)
-	if err != nil {
-		return "", err
+		log.Println("ERROR: unable to post request to zenodo", err)
+		return response, err
 	}
 	defer resp.Body.Close()
-	var rec DoiRecord
 	data, err := io.ReadAll(resp.Body)
-	err = json.Unmarshal(data, &rec)
-	if err != nil {
-		return "", err
+	if Verbose > 0 {
+		log.Println("createDocument received response", string(data))
 	}
-	arr := strings.Split(rec.Links.Bucket, "/")
-	bid := arr[len(arr)-1]
-	return bid, nil
+	err = json.Unmarshal(data, &response)
+	return response, err
 }
 
 // AddRecord represents add API to zenodo
-func AddRecord(docId int64, name string, foxdenRecord any) error {
-	initSrv()
+/*
+	curl --upload-file readme.md --request PUT
+	'https://zenodo.org/api/files/50b47f75-c97d-47c6-af11-caa6e967c1d5/readme.md?access_token=<KEY>'
+*/
+func AddRecord(docId int64, fileName string, foxdenRecord any) error {
 	data, err := json.Marshal(foxdenRecord)
 	if err != nil {
 		return err
 	}
-	bid, err := getBid(docId)
+	records, err := DoiRecords(docId)
 	if err != nil {
 		return err
 	}
-	rurl := fmt.Sprintf("%s/add/%s/%s", srvConfig.Config.Services.PublicationURL, bid, name)
-	resp, err := _httpWriteRequest.Put(rurl, "application/json", bytes.NewBuffer(data))
+	if len(records) != 1 {
+		return errors.New("Too many DOI records")
+	}
+	rec := records[0]
+	arr := strings.Split(rec.Links.Bucket, "/")
+	bucket := arr[len(arr)-1]
+
+	// create new deposit
+	zurl := srvConfig.Config.DOI.Zenodo.Url
+	token := srvConfig.Config.DOI.Zenodo.AccessToken
+	rurl := fmt.Sprintf("%s/files/%s/%s?access_token=%s", zurl, bucket, fileName, token)
+	if Verbose > 0 {
+		log.Println("request", rurl)
+	}
+
+	// place HTTP request to zenodo upstream server
+	req, err := http.NewRequest("PUT", rurl, bytes.NewReader(data))
+	if err != nil {
+		return err
+	}
+	client := http.Client{}
+	resp, err := client.Do(req)
 	defer resp.Body.Close()
-	if err != nil {
-		return err
+	data, err = io.ReadAll(resp.Body)
+	if Verbose > 0 {
+		log.Println("updateDocument received response", string(data))
 	}
-	if err := checkResponse(resp); err != nil {
-		return err
-	}
-	return nil
+	return err
 }
 
+// UpdateRecord updates Zenodo records with our metadata
+/*
+   # add mandatory metadata to our publication
+   curl -v -X PUT "https://zenodo.org/api/deposit/depositions/<ID>?access_token=<TOKEN>" \
+		   -H "Content-type: application/json" -d@meta1.json
+
+	{
+		"metadata": {
+			"publication_type": "article",
+			"upload_type":"publication",
+			"description":"This is a test",
+			"keywords": ["bla", "foo"],
+			"title":"Test"
+		}
+	}
+*/
 func UpdateRecord(docId int64, mrec MetaDataRecord) error {
-	// init reader/writer and srv config
-	initSrv()
 	rec := MetaRecord{Metadata: mrec}
 	data, err := json.Marshal(rec)
 	if err != nil {
 		return err
 	}
-	rurl := fmt.Sprintf("%s/update/%d", srvConfig.Config.Services.PublicationURL, docId)
-	resp, err := _httpWriteRequest.Put(rurl, "application/json", bytes.NewBuffer(data))
-	defer resp.Body.Close()
+	zurl := srvConfig.Config.DOI.Zenodo.Url
+	token := srvConfig.Config.DOI.Zenodo.AccessToken
+	rurl := fmt.Sprintf("%s/deposit/depositions/%d?access_token=%s", zurl, docId, token)
+
+	// place HTTP request to zenodo upstream server
+	req, err := http.NewRequest("PUT", rurl, bytes.NewReader(data))
 	if err != nil {
+		log.Println("ERROR: unable to PUT request to zenodo", err)
 		return err
 	}
+	client := http.Client{}
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
 	data, err = io.ReadAll(resp.Body)
-	if err := checkResponse(resp); err != nil {
-		return err
+	if Verbose > 0 {
+		log.Println("updateDocument received response", string(data))
 	}
-	return nil
+	return err
 }
 
+// PublishRecord publishes docId record in Zenodo
+// curl -v -X POST "https://zenodo.org/api/deposit/depositions/<ID>/actions/publish?access_token=<TOKEN>"
 func PublishRecord(docId int64) (DoiRecord, error) {
-	// init reader/writer and srv config
-	initSrv()
+	zurl := srvConfig.Config.DOI.Zenodo.Url
+	token := srvConfig.Config.DOI.Zenodo.AccessToken
+	rurl := fmt.Sprintf("%s/deposit/depositions/%d/actions/publish?access_token=%s", zurl, docId, token)
+	var record DoiRecord
 
-	var doiRecord DoiRecord
-
-	// publish the record
-	rurl := fmt.Sprintf("%s/publish/%d", srvConfig.Config.Services.PublicationURL, docId)
-	publishResp, err := _httpWriteRequest.Post(rurl, "application/json", bytes.NewBuffer([]byte{}))
-	defer publishResp.Body.Close()
+	// place HTTP request to zenodo upstream server
+	req, err := http.NewRequest("POST", rurl, nil)
 	if err != nil {
-		return doiRecord, err
+		log.Println("ERROR: unable to POST request to zenodo", err)
+		return record, err
 	}
-	if err := checkResponse(publishResp); err != nil {
-		return doiRecord, err
+	client := http.Client{}
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	data, err := io.ReadAll(resp.Body)
+	if Verbose > 0 {
+		log.Println("publishDocument received response", string(data))
 	}
+	err = json.Unmarshal(data, &record)
+	return record, err
+}
 
-	// fetch our document
-	rurl = fmt.Sprintf("%s/docs/%d", srvConfig.Config.Services.PublicationURL, docId)
-	docsResp, err := _httpReadRequest.Get(rurl)
-	defer docsResp.Body.Close()
+// DoiRecords returns list of Zenodo DOI records
+func DoiRecords(docId int64) ([]DoiRecord, error) {
+	/*
+	 curl 'https://zenodo.org/api/deposit/depositions?access_token=<KEY>'
+	 curl 'https://zenodo.org/api/deposit/depositions/<123>?access_token=<KEY>'
+	*/
+	var records []DoiRecord
+	zurl := srvConfig.Config.DOI.Zenodo.Url
+	token := srvConfig.Config.DOI.Zenodo.AccessToken
+	rurl := fmt.Sprintf("%s/deposit/depositions?access_token=%s", zurl, token)
+	if docId != 0 {
+		rurl = fmt.Sprintf("%s/deposit/depositions/%d?access_token=%s", zurl, docId, token)
+	}
+	if Verbose > 0 {
+		log.Println("request", rurl)
+	}
+	resp, err := http.Get(rurl)
 	if err != nil {
-		return doiRecord, err
+		log.Println("ERROR: in GET request", err)
+		return records, err
 	}
-	if err := checkResponse(docsResp); err != nil {
-		return doiRecord, err
+	defer resp.Body.Close()
+	dec := json.NewDecoder(resp.Body)
+	if docId == 0 {
+		if err := dec.Decode(&records); err != nil {
+			log.Println("ERROR: unable to decode JSON response", err)
+			return records, err
+		}
+		return records, nil
 	}
-	data, err := io.ReadAll(docsResp.Body)
-	if err != nil {
-		return doiRecord, err
+	var record DoiRecord
+	if err := dec.Decode(&record); err != nil {
+		log.Println("ERROR: unable to decode JSON response", err)
+		return records, err
 	}
-
-	// parse doi record
-	err = json.Unmarshal(data, &doiRecord)
-	return doiRecord, err
+	records = append(records, record)
+	return records, nil
 }
