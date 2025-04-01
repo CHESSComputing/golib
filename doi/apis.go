22ffca43fc0ea0a170e942e9a1e109f14e0440c7
Valentin Kuznetsov <vkuznet@gmail.com>
2025-04-01 08:18:43 -0400

    Add MakePublic API to Provider interface

--- doi/apis.go
+++ doi/apis.go
@@ -3,4 +3,5 @@ package doi
 // Provider represents generic DOI interface
 type Provider interface {
 	Publish(did, description string, record map[string]any, publish bool) (string, string, error)
+	MakePublic(doi string) error
 }
