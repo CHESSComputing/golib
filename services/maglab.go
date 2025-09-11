b09f069ff89352db42f77aa75464b803f7037c53
Valentin Kuznetsov <vkuznet@gmail.com>
2025-09-11 08:27:42 -0400

    Add prototype of maglab user

---
+++ services/maglab.go
@@ -0,0 +1,40 @@
+package services
+
+import "github.com/CHESSComputing/golib/ldap"
+
+// MaglabUser represents Maglab user with UserAttributes interface APIs
+type MaglabUser struct {
+	URL       string // ldap URL
+	Login     string // ldap user name
+	Password  string // ldap password
+	BaseDN    string // ldap baseDN
+	ldapCache *ldap.Cache
+}
+
+// Init function initialize MaglabUser ldap cache
+func (c *MaglabUser) Init() {
+	c.ldapCache = &ldap.Cache{Map: make(map[string]ldap.Entry)}
+}
+
+// GetUsers implement UserAttributes GetUsers API
+func (c *MaglabUser) GetUsers() ([]string, error) {
+	return ldap.GetUsers(c.URL, c.Login, c.Password, c.BaseDN)
+}
+
+// GetGroups implements UserAttributes Get API
+func (c *MaglabUser) GetGroups() ([]string, error) {
+	return ldap.GetUsers(c.URL, c.Login, c.Password, c.BaseDN)
+}
+
+// Get implements UserAttributes Get API
+func (c *MaglabUser) Get(name string) (User, error) {
+	user := User{
+		Name: name,
+	}
+	entry, err := c.ldapCache.Search(c.Login, c.Password, name)
+	if err != nil {
+		return user, err
+	}
+	user.Groups = entry.Btrs
+	return user, nil
+}
