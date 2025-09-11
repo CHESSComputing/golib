5f5b1f27dcbb3beaf9b4e95e389255f269bf3722
Valentin Kuznetsov <vkuznet@gmail.com>
2025-09-11 08:12:06 -0400

    Add functions how to get list of users and list of groups in LDAP

--- ldap/utils.go
+++ ldap/utils.go
@@ -56,3 +56,82 @@ func removeSuffix(s, suffix string) string {
 	}
 	return s
 }
+
+// GetUsers queries LDAP and returns all user CNs
+func GetUsers(ldapURL, login, password, baseDN string) ([]string, error) {
+	var users []string
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+	l, err := ldap.DialURL(ldapURL, ldap.DialWithTLSConfig(tlsConfig))
+	if err != nil {
+		return users, err
+	}
+	defer l.Close()
+
+	// connect to LDAP server
+	err = l.Bind(login, password)
+	if err != nil {
+		return users, err
+	}
+
+	// Adjust filter depending on your LDAP schema
+	// For Active Directory: "(objectClass=user)"
+	// For OpenLDAP: "(objectClass=inetOrgPerson)"
+	searchRequest := ldap.NewSearchRequest(
+		baseDN,
+		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+		"(objectClass=inetOrgPerson)",
+		[]string{"cn"},
+		nil,
+	)
+
+	result, err := l.Search(searchRequest)
+	if err != nil {
+		return users, err
+	}
+
+	for _, entry := range result.Entries {
+		users = append(users, entry.GetAttributeValue("cn"))
+	}
+	return users, nil
+}
+
+// GetGroups queries LDAP and returns all group CNs
+func GetGroups(ldapURL, login, password, baseDN string) ([]string, error) {
+	var groups []string
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+	l, err := ldap.DialURL(ldapURL, ldap.DialWithTLSConfig(tlsConfig))
+	if err != nil {
+		return groups, err
+	}
+	defer l.Close()
+
+	// connect to LDAP server
+	err = l.Bind(login, password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// For Active Directory: "(objectClass=group)"
+	// For OpenLDAP: "(objectClass=posixGroup)"
+	searchRequest := ldap.NewSearchRequest(
+		baseDN,
+		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+		"(objectClass=posixGroup)",
+		[]string{"cn"},
+		nil,
+	)
+
+	result, err := l.Search(searchRequest)
+	if err != nil {
+		return groups, err
+	}
+
+	for _, entry := range result.Entries {
+		groups = append(groups, entry.GetAttributeValue("cn"))
+	}
+	return groups, nil
+}
