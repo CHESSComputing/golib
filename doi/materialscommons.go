f7c03114dded67f3d265d41ab1edb25c5ccecfa0
Valentin Kuznetsov <vkuznet@gmail.com>
2026-03-13 08:52:54 -0400

    adjust error handling

--- doi/materialscommons.go
+++ doi/materialscommons.go
@@ -1,6 +1,8 @@
 package doi
 
 import (
+	"fmt"
+
 	srvConfig "github.com/CHESSComputing/golib/config"
 	materialscommons "github.com/CHESSComputing/golib/materialscommons"
 )
@@ -22,7 +24,10 @@ func (m *MCProvider) Init() {
 // Publish provides publication of dataset with did and description
 func (m *MCProvider) Publish(did, description string, record map[string]any, publish bool) (string, string, error) {
 	doi, doiLink, err := materialscommons.Publish(did, m.ProjectName, description, record, publish, m.Verbose)
-	return doi, doiLink, err
+	if err != nil {
+		return doi, doiLink, fmt.Errorf("[golib.doi.MCProvider.Publish] materialscommons.Publish error: %w", err)
+	}
+	return doi, doiLink, nil
 }
 
 // MakePublic provides publication of draft DOI
