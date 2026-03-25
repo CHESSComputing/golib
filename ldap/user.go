f3bbd5cc2ae32f7d7786efcf1a1f935f26dd26c3
Valentin Kuznetsov <vkuznet@gmail.com>
2026-03-25 10:51:09 -0400

    Add GetEmail API to look-up user's email from his/her name

--- ldap/user.go
+++ ldap/user.go
@@ -1,6 +1,11 @@
 package ldap
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	srvConfig "github.com/CHESSComputing/golib/config"
+)
 
 // UserInfo represents the structure returned by the user service.
 type UserInfo struct {
@@ -15,4 +20,24 @@ type UserInfo struct {
 	Beamlines []string  `json:"Beamlines"`
 	Expire    time.Time `json:"Expire"`
 	Foxdens   []string  `json:"Foxdens"`
+}
+
+// GetEmail lookup user's email for provided user name
+func GetEmail(login, password, name string) (string, error) {
+	var email string
+
+	// extract LDAP configuration parameters
+	ldapURL := srvConfig.Config.LDAP.URL
+	baseDN := srvConfig.Config.LDAP.BaseDN
+	attributes := []string{"mail"}
+	results, err := SearchBy(
+		ldapURL, login, password, baseDN, name, "cn", attributes)
+	if err != nil {
+		return email, fmt.Errorf("[golib.ldap.GetEmail] Search error: %w", err)
+	}
+	for _, entry := range results.Entries {
+		email = entry.GetAttributeValue("mail")
+		return email, nil
+	}
+	return email, nil
 }
